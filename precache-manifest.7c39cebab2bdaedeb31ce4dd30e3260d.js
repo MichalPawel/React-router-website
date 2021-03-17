898a3983733f7fcefcd5934aca13c9d90566f68b
Pawel <pawelmm96@gmail.com>
2021-03-17 21:12:08 +0100

    Updates

---
+++ precache-manifest.7c39cebab2bdaedeb31ce4dd30e3260d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd13c014da09d8b68d88",
+    "url": "/project-react-router/static/css/main.56a52b64.chunk.css"
+  },
+  {
+    "revision": "fd13c014da09d8b68d88",
+    "url": "/project-react-router/static/js/main.6251ebb6.chunk.js"
+  },
+  {
+    "revision": "28869abae223db6b2af6",
+    "url": "/project-react-router/static/js/runtime~main.f2082460.js"
+  },
+  {
+    "revision": "7571f93171147c918515",
+    "url": "/project-react-router/static/js/2.7260c2f7.chunk.js"
+  },
+  {
+    "revision": "c227b03e24cfbd1799dc342fdbb46c0d",
+    "url": "/project-react-router/static/media/gruz1.c227b03e.jpg"
+  },
+  {
+    "revision": "9cbc8df4aaff76bc6446fcb5b3333b2b",
+    "url": "/project-react-router/static/media/gruz2.9cbc8df4.jpg"
+  },
+  {
+    "revision": "ce77489676b5f6b78d2f58a022c6f8e1",
+    "url": "/project-react-router/static/media/gruz3.ce774896.jpg"
+  },
+  {
+    "revision": "9215713591adbc58429786c151175b7b",
+    "url": "/project-react-router/static/media/gruz4.92157135.jpg"
+  },
+  {
+    "revision": "4b24e8788d1f31fb3725525727b886c1",
+    "url": "/project-react-router/index.html"
+  }
+];
